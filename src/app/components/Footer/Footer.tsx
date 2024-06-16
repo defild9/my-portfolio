0b59ac31f466957a096941e38728f5e7e41fb45b
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-06-16 16:10:45 +0300

    Created footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SocialLinks from '../SocialLinks/SocialLinks'
+import './Style.css'
+
+export default function Footer() {
+  const linkArray: { [key: string]: string } = {
+    About: '#about',
+    Portfolio: '#portfolio',
+    Contact: '#contact',
+  }
+  return (
+    <footer className="flex items-center justify-between px-40">
+      <div>
+        <SocialLinks />
+      </div>
+      <div className="flex gap-10">
+        {Object.keys(linkArray).map((key) => (
+          <a key={key} className="font-light text-xl" href={linkArray[key]}>
+            {key}
+          </a>
+        ))}
+      </div>
+    </footer>
+  )
+}
