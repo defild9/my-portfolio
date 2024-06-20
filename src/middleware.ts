b8c72bd296916d1754dc1d29aa728bd66370462e
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-06-21 00:59:04 +0300

    Update private routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/admin', '/admin/skills'] }
+export const config = {
+  matcher: [
+    '/admin',
+    '/admin/skills',
+    '/admin/portfolio',
+    '/admin/experience',
+    '/admin/contact',
+  ],
+}
