dcee0d99b36714914f728b39fe1f9ff04c3504f0
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-06-23 22:20:55 +0300

    Added props for about section

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-export default function AboutSection() {
+interface AboutSectionProps {
+  text: string
+}
+
+export default function AboutSection({ text }: AboutSectionProps) {
   return (
-    <div id="about" className="flex flex-col px-6 md:px-20 lg:px-40 py-10 md:py-20">
-      <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-5">About</h2>
-      <p className="text-base md:text-lg text-justify">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius debitis
-        illo consectetur consequuntur, id perferendis soluta blanditiis
-        cupiditate recusandae pariatur fuga minus, non porro! Eum commodi neque
-        quibusdam at repellendus.
-      </p>
+    <div
+      id="about"
+      className="flex flex-col px-6 md:px-20 lg:px-40 py-10 md:py-20"
+    >
+      <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-5">
+        About
+      </h2>
+      <p className="text-base md:text-lg text-justify">{text}</p>
     </div>
   )
 }
